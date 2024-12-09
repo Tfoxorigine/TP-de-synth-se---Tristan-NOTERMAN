c72863ba814fbc9babffd08f7e2cb2ce2a52b7f3
Tristan <47038656+Tfoxorigine@users.noreply.github.com>
2024-12-09 12:18:13 +0100

    Delete la mauvaise partie du tp

--- tp de synthèse/execution.h
+++
@@ -1,7 +0,0 @@
-#ifndef EXECUTION_H
-#define EXECUTION_H
-
-// Déclaration de la fonction d'exécution des commandes
-void execute_command(const char *command);
-
-#endif // EXECUTION_H
--- tp de synthèse/prompt.h
+++
@@ -1,7 +0,0 @@
-#ifndef PROMPT_H
-#define PROMPT_H
-
-void display_welcome_message();
-char* get_prompt_input();
-
-#endif // PROMPT_H
